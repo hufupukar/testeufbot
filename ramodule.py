d0bab7ba5b12eb074c1bb550f13ed7ed94f2a36f
Fernando <fernandofreitastwi@gmail.com>
2017-11-14 23:16:03 -0200

    Estou inserindo os testes a respeito da funcionalidade do meu bot

---
+++ ramodule.py
@@ -0,0 +1,10 @@
+import openpyxl
+from openpyxl import load_workbook
+excel_document = openpyxl.load_workbook("C:/Users/Fernando/testeufbot/tabula-2_ajuste_matriculas_2017.3_matriculas_por_aluno.xlsx")
+sheet = excel_document.get_sheet_by_name('Worksheet')
+def ra_find(RA):
+    for i in range(2, 42650):
+    if RA = sheet.cell(row=i, column=1).value
+        teste=sheet.cell(row=i, column=3).value
+        teste.encode('utf-8')
+        print(teste.encode('utf-8'))
---
+++ testeaula.py
@@ -0,0 +1,46 @@
+import telepot
+import openpyxl
+from openpyxl import load_workbook
+excel_document = openpyxl.load_workbook("C:/Users/Fernando/testeufbot/tabula-2_ajuste_matriculas_2017.3_matriculas_por_aluno.xlsx")
+sheet = excel_document.get_sheet_by_name('Worksheet')
+
+
+bot = telepot.Bot('474490928:AAEJVbHe0Rb5jx3sk1RDcqwbgMb8LnaQGHw')
+
+
+def ModRecebeMsg(msg):
+#modulo de processamendo de mensagens recebidas
+	content_type, chat_type, chat_id = telepot.glance(msg)
+	print(content_type, chat_type, chat_id)
+ 	if content_type == 'text':
+            for i in range (2,42650):
+                ra=sheet.cell(row=i, column=1).value
+                if ra=='text': #Aqui está onde não consigo saber o que fazer,
+                               #eu preciso que o RA seja igual ao texto inserido
+                               #(supostamente o RA da pessoa)
+                    bot.sendMessage(chat_id, ra)
+
+bot.message_loop(ModRecebeMsg)
+while True:
+    pass
+
+
+'''
+#Se eu uso meu código da seguinte forma:
+
+        def ModRecebeMsg(msg):
+        #modulo de processamendo de mensagens recebidas
+        	content_type, chat_type, chat_id = telepot.glance(msg)
+        	print(content_type, chat_type, chat_id)
+         	if content_type == 'text':
+                    for i in range (2,42650):
+                        ra=sheet.cell(row=i, column=1).value
+                        bot.sendMessage(chat_id, ra)
+
+        bot.message_loop(ModRecebeMsg)
+        while True:
+            pass
+
+ele irá varrer todo o pdf e me mostrando todos os RA's, mas eu preciso que ele só me mostre quando
+o RA for = ao inserido
+'''
