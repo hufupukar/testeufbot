df26543fe9bff71e3d401d7bc47e0aed69e0035b
Fernando <fernandofreitastwi@gmail.com>
2017-11-16 20:23:24 -0200

    inserindo arquivos

--- ramodule.py
+++ ramodule.py
@@ -2,7 +2,7 @@ import openpyxl
 from openpyxl import load_workbook
 excel_document = openpyxl.load_workbook("C:/Users/Fernando/testeufbot/tabula-2_ajuste_matriculas_2017.3_matriculas_por_aluno.xlsx")
 sheet = excel_document.get_sheet_by_name('Worksheet')
-def ra_find(RA):
+def ra_find(RA): #Teste RA ALUNO
     for i in range(2, 42650):
     if RA = sheet.cell(row=i, column=1).value
         teste=sheet.cell(row=i, column=3).value
