381c3c0390ef76123b4799fe1ab239a7cbd6aefc
Fernando <fernandofreitastwi@gmail.com>
2017-11-24 12:17:27 -0200

    Alterando o repositório da tabela de acesso de turmas para o GitHub

--- testeaula.py
+++ testeaula.py
@@ -2,7 +2,7 @@ import os
 import telepot
 import openpyxl
 from openpyxl import load_workbook
-excel_document = openpyxl.load_workbook("C:/Users/Fernando/testeufbot/tabula-2_ajuste_matriculas_2017.3_matriculas_por_aluno.xlsx")
+excel_document = openpyxl.load_workbook("https://github.com/hufupukar/testeufbot/blob/master/tabula-2_ajuste_matriculas_2017.3_matriculas_por_aluno.xlsx")
 sheet = excel_document.get_sheet_by_name('Worksheet')
 
 
